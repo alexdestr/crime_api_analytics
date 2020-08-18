f8b6ebb2d285cbe4b1c129e0dee1621b6556eef2
VincentVega <alexdestr@gmail.com>
2020-08-18 14:23:10 +0300

    -Second row was reworked.

--- db/scripts/script#2/crimeVolumeComparsion.sql
+++ db/scripts/script#2/crimeVolumeComparsion.sql
@@ -1,31 +1,37 @@
 WITH crimes_count AS
 (SELECT *
- FROM
- (SELECT *
   FROM
-  (SELECT category,
-          MONTH AS mth,
-          COUNT(*) OVER(PARTITION BY category, MONTH
-          ORDER BY MONTH)
-   FROM streetlevelcrimes) AS a
-  GROUP BY a.category,
-  a.mth,
-  a.count
-  ORDER BY a.mth) AS b),
+  (SELECT
+    category,
+    MONTH AS mth,
+    COUNT(*)
+    OVER(PARTITION BY category, MONTH
+    ORDER BY MONTH)
+   FROM streetlevelcrimes) AS count_for_each_category
+  GROUP BY
+  count_for_each_category.category,
+  count_for_each_category.mth,
+  count_for_each_category.count
+  ORDER BY count_for_each_category.mth
+),
+
 odr AS
-(SELECT b.category,
-        b.mth,
-        a.count AS previous_month_count,
-        b.count AS current_month_count
- FROM crimes_count AS a
-   JOIN crimes_count AS b ON a.category = b.category
- WHERE a.mth = to_char(to_date(b.mth, 'YYYY-MM') - INTERVAL '1 month', 'YYYY-MM') )
-SELECT category,
-       mth,
-       previous_month_count,
-       current_month_count,
-       current_month_count - previous_month_count AS delta,
-       concat(cast(round((cast(current_month_count AS decimal) - cast(previous_month_count AS decimal)) / cast(previous_month_count AS decimal) * 100, 1) AS text), '%') AS growth_rate
+(SELECT
+  current_month_data.category,
+  current_month_data.mth,
+  last_month_data.count AS previous_month_count,
+  current_month_data.count AS current_month_count
+ FROM crimes_count AS last_month_data
+   JOIN crimes_count AS current_month_data ON last_month_data.category = current_month_data.category
+ WHERE last_month_data.mth = to_char(to_date(current_month_data.mth, 'YYYY-MM') - INTERVAL '1 month', 'YYYY-MM') )
+
+SELECT
+  category,
+  mth,
+  previous_month_count,
+  current_month_count,
+  current_month_count - previous_month_count AS delta,
+  concat(cast(round((cast(current_month_count AS decimal) - cast(previous_month_count AS decimal)) / cast(previous_month_count AS decimal) * 100, 1) AS text), '%') AS growth_rate
 FROM odr
 WHERE TO_DATE(mth, 'YYYY-MM') >= TO_DATE(''|| ? ||'', 'YYYY-MM')
 AND TO_DATE(mth, 'YYYY-MM') <= TO_DATE(''|| ? ||'', 'YYYY-MM')
