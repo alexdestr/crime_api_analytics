3b406fea22a424f5aeb379e76049c34bec2a95f3
VincentVega <alexdestr@gmail.com>
2020-08-18 14:30:04 +0300

    -Third row was ramade.

--- db/scripts/script#3/crimesWithSpecifiedOutcomeStatus.sql
+++ db/scripts/script#3/crimesWithSpecifiedOutcomeStatus.sql
@@ -1,5 +1,11 @@
 WITH count_crime_with_current_category AS (
-  SELECT street_id, street_name, outcome_category, month, COUNT(*) AS count FROM streetlevelcrimes
+  SELECT
+  street_id,
+  street_name,
+  outcome_category,
+  month,
+  COUNT(*) AS count
+  FROM streetlevelcrimes
   WHERE outcome_category = (''|| ? ||'')
   GROUP BY street_id, street_name, outcome_category, month
   HAVING TO_DATE(month, 'YYYY-MM') >= TO_DATE(''|| ? ||'', 'YYYY-MM')
@@ -7,15 +13,28 @@ WITH count_crime_with_current_category AS (
 ),
 
 count_crime_with_other_category AS (
-  SELECT street_id, street_name, month, COUNT(*) AS count FROM streetlevelcrimes
+  SELECT
+  street_id,
+  street_name,
+  month,
+  COUNT(*) AS count
+  FROM streetlevelcrimes
   WHERE outcome_category IS NOT NULL
   GROUP BY street_id, street_name, month
   HAVING TO_DATE(month, 'YYYY-MM') >= TO_DATE(''|| ? ||'', 'YYYY-MM')
   AND TO_DATE(month, 'YYYY-MM') <= TO_DATE(''|| ? ||'', 'YYYY-MM')
 )
 
-SELECT a.street_id, a.street_name, a.outcome_category, concat(round(cast(a.count AS decimal) / cast(b.count AS decimal) * 100.0, 1), '%') AS percentage_of_the_total_crimes FROM
-count_crime_with_current_category AS a
-JOIN count_crime_with_other_category AS b
-ON a.street_id = b.street_id AND a.month = b.month
-GROUP BY a.street_id, a.street_name, a.outcome_category, percentage_of_the_total_crimes
+SELECT
+current_count.street_id,
+current_count.street_name,
+current_count.outcome_category,
+concat(round(cast(current_count.count AS decimal) / cast(other_count.count AS decimal) * 100.0, 1), '%') AS percentage_of_the_total_crimes FROM
+count_crime_with_current_category AS current_count
+JOIN count_crime_with_other_category AS other_count
+ON current_count.street_id = other_count.street_id AND current_count.month = other_count.month
+GROUP BY
+current_count.street_id,
+current_count.street_name,
+current_count.outcome_category,
+percentage_of_the_total_crimes
