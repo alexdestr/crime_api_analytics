19ceb28d5c7be703304c3dff33c22d6b1a88431f
VincentVega <alexdestr@gmail.com>
2020-08-18 15:19:38 +0300

    -Fourth row was ramade.

--- db/scripts/script#3/crimesWithSpecifiedOutcomeStatus.sql
+++ db/scripts/script#3/crimesWithSpecifiedOutcomeStatus.sql
@@ -1,40 +1,47 @@
 WITH count_crime_with_current_category AS (
   SELECT
-  street_id,
-  street_name,
-  outcome_category,
-  month,
-  COUNT(*) AS count
+    street_id,
+    street_name,
+    outcome_category,
+    month,
+    COUNT(*) AS count
   FROM streetlevelcrimes
   WHERE outcome_category = (''|| ? ||'')
-  GROUP BY street_id, street_name, outcome_category, month
+  GROUP BY
+    street_id,
+    street_name,
+    outcome_category,
+    month
   HAVING TO_DATE(month, 'YYYY-MM') >= TO_DATE(''|| ? ||'', 'YYYY-MM')
   AND TO_DATE(month, 'YYYY-MM') <= TO_DATE(''|| ? ||'', 'YYYY-MM')
 ),
 
 count_crime_with_other_category AS (
   SELECT
-  street_id,
-  street_name,
-  month,
+    street_id,
+    street_name,
+    month,
   COUNT(*) AS count
   FROM streetlevelcrimes
   WHERE outcome_category IS NOT NULL
-  GROUP BY street_id, street_name, month
+  GROUP BY
+    street_id,
+    street_name,
+    month
   HAVING TO_DATE(month, 'YYYY-MM') >= TO_DATE(''|| ? ||'', 'YYYY-MM')
   AND TO_DATE(month, 'YYYY-MM') <= TO_DATE(''|| ? ||'', 'YYYY-MM')
 )
 
 SELECT
-current_count.street_id,
-current_count.street_name,
-current_count.outcome_category,
+  current_count.street_id,
+  current_count.street_name,
+  current_count.outcome_category,
 concat(round(cast(current_count.count AS decimal) / cast(other_count.count AS decimal) * 100.0, 1), '%') AS percentage_of_the_total_crimes FROM
 count_crime_with_current_category AS current_count
 JOIN count_crime_with_other_category AS other_count
 ON current_count.street_id = other_count.street_id AND current_count.month = other_count.month
 GROUP BY
-current_count.street_id,
-current_count.street_name,
-current_count.outcome_category,
-percentage_of_the_total_crimes
+  current_count.street_id,
+  current_count.street_name,
+  current_count.outcome_category,
+  percentage_of_the_total_crimes
--- db/scripts/script#4/stopAndSearchesStatisticByEthnicity.sql
+++ db/scripts/script#4/stopAndSearchesStatisticByEthnicity.sql
@@ -1,45 +1,89 @@
 WITH count_by_date AS (
-  SELECT officer_defined_ethnicity, datetime, outcome, COUNT(*)
+  SELECT
+    officer_defined_ethnicity,
+    datetime,
+    outcome,
+    COUNT(*)
   FROM stopandsearchesbyforce
-  GROUP BY officer_defined_ethnicity, datetime, outcome
+  GROUP BY
+    officer_defined_ethnicity,
+    datetime,
+    outcome
   HAVING TO_DATE(to_char(datetime, 'YYYY-MM'), 'YYYY-MM') >= TO_DATE(''|| ? ||'', 'YYYY-MM')
   AND TO_DATE(to_char(datetime, 'YYYY-MM'), 'YYYY-MM') <= TO_DATE(''|| ? ||'', 'YYYY-MM')
 ),
 
 arrest_count AS (
-  SELECT officer_defined_ethnicity, COUNT(*) FROM count_by_date
+  SELECT
+    officer_defined_ethnicity,
+  COUNT(*)
+  FROM count_by_date
   WHERE outcome = 'Arrest'
-  GROUP BY officer_defined_ethnicity
+  GROUP BY
+    officer_defined_ethnicity
 ),
 
 release_count AS (
-  SELECT officer_defined_ethnicity, COUNT(*) FROM count_by_date
+  SELECT
+    officer_defined_ethnicity,
+  COUNT(*)
+  FROM count_by_date
   WHERE outcome = 'A no further action disposal'
-  GROUP BY officer_defined_ethnicity
+  GROUP BY
+    officer_defined_ethnicity
 ),
 
 other_count AS (
-  SELECT officer_defined_ethnicity, COUNT(*) FROM count_by_date
+  SELECT
+    officer_defined_ethnicity,
+    COUNT(*) FROM count_by_date
   WHERE outcome != 'Arrest' AND outcome != 'A no further action disposal' AND outcome IS NOT NULL
-  GROUP BY officer_defined_ethnicity
+  GROUP BY
+    officer_defined_ethnicity
 ),
 
 object_of_search AS (
-  SELECT officer_defined_ethnicity, object_of_search, "count",
-         ROW_NUMBER() OVER (PARTITION BY officer_defined_ethnicity ORDER BY "count" DESC) AS rn
-  FROM (SELECT officer_defined_ethnicity, object_of_search, COUNT(*) FROM stopandsearchesbyforce GROUP BY officer_defined_ethnicity, object_of_search
-  ) AS a
+  SELECT
+    officer_defined_ethnicity,
+    object_of_search,
+    "count",
+    ROW_NUMBER() OVER (PARTITION BY officer_defined_ethnicity ORDER BY "count" DESC) AS rn
+  FROM (SELECT
+          officer_defined_ethnicity,
+          object_of_search,
+          COUNT(*)
+        FROM stopandsearchesbyforce
+        GROUP BY
+          officer_defined_ethnicity,
+          object_of_search
+  ) AS grouped_count
 )
 
-SELECT a.officer_defined_ethnicity, a.count, concat(round(cast(b.count AS decimal) / (cast(a.count AS decimal)) * 100, 1), '%') AS arrest_rate, concat(round(cast(c.count AS decimal) / (cast(a.count AS decimal)) * 100, 1), '%') AS release_count, concat(round(cast(d.count AS decimal) / (cast(a.count AS decimal)) * 100, 1), '%') AS other_outcomes_count, f.object_of_search
+SELECT
+  common_count.officer_defined_ethnicity,
+  common_count.count,
+  concat(round(cast(arrest_outcome.count AS decimal) / (cast(common_count.count AS decimal)) * 100, 1), '%') AS arrest_rate,
+  concat(round(cast(release_outcome.count AS decimal) / (cast(common_count.count AS decimal)) * 100, 1), '%') AS release_count,
+  concat(round(cast(other_outcome.count AS decimal) / (cast(common_count.count AS decimal)) * 100, 1), '%') AS other_outcomes_count,
+  object.object_of_search
 FROM
-(SELECT a.officer_defined_ethnicity, COUNT(*)
- FROM count_by_date AS a
- GROUP BY a.officer_defined_ethnicity)
-AS a
-  JOIN arrest_count AS b ON a.officer_defined_ethnicity = b.officer_defined_ethnicity
-  JOIN release_count AS c ON b.officer_defined_ethnicity = c.officer_defined_ethnicity
-  JOIN other_count AS d ON c.officer_defined_ethnicity = d.officer_defined_ethnicity
-  JOIN object_of_search AS f ON d.officer_defined_ethnicity = f.officer_defined_ethnicity
-GROUP BY a.officer_defined_ethnicity, a.count, arrest_rate, release_count, other_outcomes_count, f.object_of_search, f.rn
-HAVING f.rn = 1
+(SELECT
+ grouped_count_by_date.officer_defined_ethnicity,
+  COUNT(*)
+ FROM count_by_date AS grouped_count_by_date
+ GROUP BY
+ grouped_count_by_date.officer_defined_ethnicity)
+AS common_count
+  JOIN arrest_count AS arrest_outcome ON common_count.officer_defined_ethnicity = arrest_outcome.officer_defined_ethnicity
+  JOIN release_count AS release_outcome ON arrest_outcome.officer_defined_ethnicity = release_outcome.officer_defined_ethnicity
+  JOIN other_count AS other_outcome ON release_outcome.officer_defined_ethnicity = other_outcome.officer_defined_ethnicity
+  JOIN object_of_search AS object ON other_outcome.officer_defined_ethnicity = object.officer_defined_ethnicity
+GROUP BY
+  common_count.officer_defined_ethnicity,
+  common_count.count,
+  arrest_rate,
+  release_count,
+  other_outcomes_count,
+  object.object_of_search,
+  object.rn
+HAVING object.rn = 1
